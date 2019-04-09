c94dfcb4c9d5b1f34572fa2fee00afff13ea51e0
herbtea30 <herbtea30@gmail.com>
2019-04-09 13:38:38 +0900

    @RestController

--- src/main/java/com/herbtea/study/springbootstudy/user/UserController.java
+++ src/main/java/com/herbtea/study/springbootstudy/user/UserController.java
@@ -1,10 +1,9 @@
 package com.herbtea.study.springbootstudy.user;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
 
     @GetMapping("/user/info")
